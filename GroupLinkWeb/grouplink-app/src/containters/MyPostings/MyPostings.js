5e1b92ff67732a45c48663570ca005f2b42f3f5f
Asder98 <kuba.grzybek@gmail.com>
2021-04-04 18:47:03 +0200

    Added routing and views placeholders

---
+++ GroupLinkWeb/grouplink-app/src/containters/MyPostings/MyPostings.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MyPostings = ({ match }) => {
+
+  console.log(match)
+
+  return (
+    <div>
+      Mypostings
+    </div>
+  );
+};
+
+export default MyPostings;
---
+++ GroupLinkWeb/grouplink-app/src/containters/Search/Search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Search = ({match}) => {
+
+    console.log(match)
+
+    return (
+        <div>
+            <p>
+            Search
+            </p>
+        </div>
+    )
+}
+
+export default Search
